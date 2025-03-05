f4f4587e9a2e96f1f2669af103358846008662be
userom11 <126608941+userom11@users.noreply.github.com>
2025-03-05 10:12:36 +0200

    new backend with view implemented

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os,  json, time, re
-from datetime import datetime deltatime
+from datetime import datetime
 from http.server import SimpleHTTPRequestHandler, HTTPServer, BaseHTTPRequestHandler
 
 def getime(type):
@@ -13,34 +13,31 @@ def getime(type):
         if timesecs >= terms[n] and timesecs < terms[n+1]:
             current_term=n # term 1 to 4 (programming numbers, 0 to 3)
             break
-
     january_first = datetime(year, 1, 1).weekday()
     termstartday =terms[n]//86400
     currentday =(time.time())//86400
     p = (termstartday+january_first)%7
     current_term_day = currentday - termstartday +1
     current_term_week = (current_term_day -p +1)/7
-    
     periods =( json.loads( re.search(r'\{.*\}', open("backend_static_files/periodtimes.js", "r").read() ).group(0)) )["times"]
     for x in range(0,12):
         if timesecs >= periods[x] and timesecs < periods[x+1]:
             current_period=x # period 1 to 13 (breaks, assembly, block session counts as a period) (programming number system: 0 to 12)
             break
-
     if type=="termweek":
         return termweek
     if type=="current_period":
         if current_period:
             return current_period
 
-getime()
+
 def logattendence(data):
     print(data)
     termweek = getime("termweek")
     currentperiod = getime("currentperiod")
     weekday = now.weekday()
     filetowrite = open(f"backend_dyna_files/week{termweek}/{weekday+1}/c1p1.js", "w")
-    filetowrite.write("const=presence"+" "+json.dumps(data))
+    filetowrite.write("const presence="+" "+json.dumps(data))
 
 class main(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -48,21 +45,33 @@ class main(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            with open('/home/o/Digital_Attendence_System/frontend_files/room1.htm', 'rb') as file:
+            with open('frontend_files/room1.htm', 'rb') as file:
+                self.wfile.write(file.read())
+        if self.path == '/view':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            with open ('frontend_files/view.htm', 'rb') as file:
                 self.wfile.write(file.read())
         if self.path == '/class1.js':
             self.send_response(200)
             self.send_header('Content-type', 'application/javascript')
             self.end_headers()
-            with open('/home/o/Digital_Attendence_System/backend_static_files/class1.js', 'rb') as file:
+            with open('backend_static_files/class1.js', 'rb') as file:
                 self.wfile.write(file.read())
-
+        if self.path == '/todayclass1':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/javascript')
+            self.end_headers()
+            # with open(f"backend_dyna_files/week{termweek}/{weekday+1}/c1p{current_period}.js", 'rb') as file:
+            with open("backend_dyna_files/c1p2.js", 'rb') as file:
+                self.wfile.write(file.read())
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data.decode('utf-8'))
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'application/javascript')
         self.end_headers()
         response = {'status': 'success', 'received': data}
         self.wfile.write(json.dumps(response).encode('utf-8'))
